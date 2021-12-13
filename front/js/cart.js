fd178804162df781b8273c1c58c179f1b35529ce
Colli Gregory <colli.gregory.13@gmail.com>
2021-12-13 20:06:18 +0100

    fix: modif rendu html

--- front/js/cart.js
+++ front/js/cart.js
@@ -12,68 +12,71 @@ let quantiteTotal = 0;
 
 console.log(panier);
 
-panier.forEach((element, index) => {
-    renderHTML(element);
-});
 
-function renderHTML(element) {
-    sectionPanier.innerHTML +=
-        `<article class="cart__item" data-id="${element.id}" data-color="${element.couleurChoisie}">
-            <div class="cart__item__img">
-              <img src="${element.imageUrl}" alt="${element.altText}">
-            </div>
-            <div class="cart__item__content">
-              <div class="cart__item__content__description">
-                <h2>${element.nom}</h2>
-                <p>${element.couleurChoisie}</p>
-                <p>${element.prix} €</p>
-              </div>
-              <div class="cart__item__content__settings">
-                <div class="cart__item__content__settings__quantity">
-                  <p>Qté : </p>
-                  <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${element.nbre}">
-                </div>
-                <div class="cart__item__content__settings__delete">
-                  <p data-id="${element.id}" data-color="${element.couleurChoisie}" class="deleteItem">Supprimer</p>
-                </div>
-              </div>
-            </div>
-          </article>`;
+renderHTML(panier);
 
-    prixTotal += element.prix * element.nbre;
-    quantiteTotal += element.nbre;
-    ajoutEvent();
+
+function renderHTML(panier) {
+
+    panier.forEach(element => {
+        sectionPanier.innerHTML +=
+            `<article class="cart__item" data-id="${element.id}" data-color="${element.couleurChoisie}">
+        <div class="cart__item__img">
+          <img src="${element.imageUrl}" alt="${element.altText}">
+        </div>
+        <div class="cart__item__content">
+          <div class="cart__item__content__description">
+            <h2>${element.nom}</h2>
+            <p>${element.couleurChoisie}</p>
+            <p>${element.prix} €</p>
+          </div>
+          <div class="cart__item__content__settings">
+            <div class="cart__item__content__settings__quantity">
+              <p>Qté : </p>
+              <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${element.nbre}">
+            </div>
+            <div class="cart__item__content__settings__delete">
+              <p data-id="${element.id}" data-color="${element.couleurChoisie}" class="deleteItem">Supprimer</p>
+            </div>
+          </div>
+        </div>
+      </article>`;
+
+        prixTotal += element.prix * element.nbre;
+        quantiteTotal += element.nbre;
+        ajoutEvent();
+    })
+
+
+    spanPrixTotal.innerHTML = prixTotal;
+    spanQuantiteTotalArticle.innerHTML = quantiteTotal;
 }
 
-spanPrixTotal.innerHTML = prixTotal;
-spanQuantiteTotalArticle.innerHTML = quantiteTotal;
 
 
 
-function ajoutEvent(){
+
+function ajoutEvent() {
     let btnSupprimer = document.querySelectorAll('.deleteItem');
 
     btnSupprimer.forEach(element => {
         element.addEventListener('click', (e) => {
-    
+
             console.log(e.target.dataset);
             supprimerElementPanier(e.target.dataset.id, e.target.dataset.color);
-    
-            let newPanier = getPanier()
-            let newPrixTotal = 0;
-            let newQuantiteTotal = 0;
 
-    
+            let newPanier = getPanier()
+            prixTotal = 0;
+            quantiteTotal = 0;
+
+
             sectionPanier.innerHTML = '';
-            newPanier.forEach(element => {
-                renderHTML(element);
-                newPrixTotal += element.prix * element.nbre;
-                newQuantiteTotal += element.nbre;
-            });
-    
-            spanPrixTotal.innerHTML = newPrixTotal;
-            spanQuantiteTotalArticle.innerHTML = newQuantiteTotal;
-    
+
+            renderHTML(newPanier);
+
+
+
+
         });
     });
 }
